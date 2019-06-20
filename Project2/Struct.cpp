a66a2d46788316864ba94e6856450034805f4a5d
Aleksey <aleks-sh@mail.ru>
2019-06-20 14:51:36 +0300

    3.3.11 Конструктор деструктор

--- Project2/Struct.cpp
+++ Project2/Struct.cpp
@@ -11,3 +11,28 @@ double lenght(Segment *s) {
 int & IntArray2D::get(size_t i, size_t j) {
 	return data[i*b + j];
 }
+struct String {
+	/* Реализуйте этот конструктор */
+	String(const char *str = "") {
+		size_t size = 0;
+		while (*(str + size) != '\0')
+			size++;
+		this->size = size;
+		this->str = new char[this->size + 1];
+		size_t i = 0;
+		for (; i < this->size; i++)
+			*(this->str + i) = *(str + i);
+		*(this->str + i) = '\0';
+	}
+	/* Реализуйте этот конструктор */
+	String(size_t n, char c) :size(n) {
+		this->str = new char[n + 1];
+		for (size_t i = 0; i < n; i++) *(this->str + i) = c;
+		*(this->str + n) = '\0';
+	}
+	~String() {
+		delete[] str;
+	}
+	size_t size;
+	char *str;
+};
