6e36e04c46e3f29da5df544fc7a25efa2beb3f6c
Aleksey <aleks-sh@mail.ru>
2019-06-23 22:46:05 +0300

    OOP

---
+++ OOP/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <clocale>
+#include <windows.h>
+#include "OOP.h"
+using namespace std;
+
+
+
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+
+
+
+	cin.get();
+	cout << "\n Press Enter";
+	cin.get();
+	return 0;
+}
